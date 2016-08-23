2e3b24d4f010503c799226434adce9d5289b1e42
r7kamura <r7kamura@gmail.com>
2016-08-23 22:48:08 +0900

    Use double-quote for normal String literal

--- index.ios.js
+++ index.ios.js
@@ -4,13 +4,13 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from "react-native";
 
 class amakani extends Component {
   render() {
@@ -23,7 +23,7 @@ class amakani extends Component {
           To get started, edit index.ios.js
         </Text>
         <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
+          Press Cmd+R to reload,{"\n"}
           Cmd+D or shake for dev menu
         </Text>
       </View>
@@ -34,20 +34,20 @@ class amakani extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5,
   },
 });
 
-AppRegistry.registerComponent('amakani', () => amakani);
+AppRegistry.registerComponent("amakani", () => amakani);
